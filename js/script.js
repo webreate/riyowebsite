29eee5cfbedf99bda8d1ee18501c17864a396c25
Neha <neha@webreate.com>
2023-02-14 17:06:44 +0530

    added header and footer to all pages

--- js/script.js
+++ js/script.js
@@ -1,59 +1,49 @@
-// Responsive navbar
-
-let menu = document.querySelector(".icon");
-let navbar = document.querySelector(".navbar");
-
-if (menu && navbar) {
-  menu.onclick = () => {
-    menu.classList.toggle("bx-x");
-    navbar.classList.toggle("active");
-  };
-  window.onscroll = () => {
-    menu.classList.remove("bx-x");
-    navbar.classList.remove("active");
-  };
-}
-
-var btn1 = document.getElementById("btn1");
-var btn2 = document.getElementById("btn2");
-var btn3 = document.getElementById("btn3");
-var div1 = document.getElementById("boxRight");
-var div2 = document.getElementById("box5-right2");
-var div3 = document.getElementById("box5-right3");
-
-if (btn1 && btn2 && btn3 && div1 && div2 && div3) {
-  btn1.addEventListener("click", () => {
-    btn1.classList.add("active3");
-    btn2.classList.remove("active3");
-    btn3.classList.remove("active3");
-    div1.style.display = "flex";
-    div2.style.display = "none";
-    div3.style.display = "none";
-  });
-  btn2.addEventListener("click", () => {
-    btn1.classList.remove("active3");
-    btn2.classList.add("active3");
-    btn3.classList.remove("active3");
-    div1.style.display = "none";
-    div2.style.display = "flex";
-    div3.style.display = "none";
-  });
-  btn3.addEventListener("click", () => {
-    btn1.classList.remove("active3");
-    btn2.classList.remove("active3");
-    btn3.classList.add("active3");
-    div1.style.display = "none";
-    div2.style.display = "none";
-    div3.style.display = "flex";
-  });
-}
-
 // $("#mobile_code").intlTelInput({
 // 	initialCountry: "in",
 // 	separateDialCode: true,
 // 	// utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/11.0.4/js/utils.js"
 // })
 $(document).ready(function () {
+  // Responsive navbar
+
+  $(document).on("click", ".icon", ()=>{
+    $('.icon').toggleClass("bx-x");
+    $('.navbar').toggleClass("active");
+  })
+
+  var btn1 = document.getElementById("btn1");
+  var btn2 = document.getElementById("btn2");
+  var btn3 = document.getElementById("btn3");
+  var div1 = document.getElementById("boxRight");
+  var div2 = document.getElementById("box5-right2");
+  var div3 = document.getElementById("box5-right3");
+
+  if (btn1 && btn2 && btn3 && div1 && div2 && div3) {
+    btn1.addEventListener("click", () => {
+      btn1.classList.add("active3");
+      btn2.classList.remove("active3");
+      btn3.classList.remove("active3");
+      div1.style.display = "flex";
+      div2.style.display = "none";
+      div3.style.display = "none";
+    });
+    btn2.addEventListener("click", () => {
+      btn1.classList.remove("active3");
+      btn2.classList.add("active3");
+      btn3.classList.remove("active3");
+      div1.style.display = "none";
+      div2.style.display = "flex";
+      div3.style.display = "none";
+    });
+    btn3.addEventListener("click", () => {
+      btn1.classList.remove("active3");
+      btn2.classList.remove("active3");
+      btn3.classList.add("active3");
+      div1.style.display = "none";
+      div2.style.display = "none";
+      div3.style.display = "flex";
+    });
+  }
   LottieInteractivity.create({
     mode: "scroll",
     player: "#lottie-station",
