3854449c93169bae83e59267b9af37b5aa60680f
Shivam <shiv240898@gmail.com>
2023-03-29 16:50:34 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -97,3 +97,18 @@ AOS.init({
   once: false,
   mirror: false,
 });
+
+
+const dd = document.querySelector('#dropdown-wrapper');
+const links = document.querySelectorAll('.dropdown-list a');
+const span = document.querySelector('span');
+
+dd.addEventListener('click', function() {
+  this.classList.toggle('is-active');
+});
+
+links.forEach((element) => {
+  element.addEventListener('click', function(evt) {
+    span.innerHTML = evt.currentTarget.textContent;
+  })
+})
